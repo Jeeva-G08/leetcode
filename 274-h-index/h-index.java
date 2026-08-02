7aa66948ba460b2c0fadd30765ca33f1efb99a73
Jeeva-G08 <jeevagkk@gmail.com>
2026-08-02 14:26:13 +0530

    Time: 5 ms (73.27%) | Memory: 43.6 MB (44.37%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,17 +1,11 @@
 import java.util.Arrays;
-
-class Solution {
-    public int hIndex(int[] citations) {
-
+class Solution{
+    public int hIndex(int[] citations){
         Arrays.sort(citations);
-
         int n = citations.length;
-
-        for (int i = 0; i < n; i++) {
-
-            int h = n - i;
-
-            if (citations[i] >= h) {
+        for (int i=0; i<n; i++){
+             int h=n-i;
+             if (citations[i]>=h){
                 return h;
             }
         }
